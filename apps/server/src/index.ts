ed0154c58edd1bafcf3afc8ede05f5993b165929
Alexandru Cretu <cretu.alexandru2000@gmail.com>
2025-05-25 01:26:53 +0300

    env validations

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { RPCHandler } from "@orpc/server/fetch";
 import { Hono } from "hono";
+import "dotenv/config";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 import { auth } from "./lib/auth";
