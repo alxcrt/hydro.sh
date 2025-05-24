0eda835115db036af89ec3621bc66ef0ecce1f61
Alexandru Cretu <cretu.alexandru2000@gmail.com>
2025-05-24 22:32:37 +0300

    fix typecheck errors

--- apps/web/src/components/loader.tsx
+++ apps/web/src/components/loader.tsx
@@ -1,9 +1,8 @@
-import { Loader2 } from "lucide-react";
-
 export default function Loader() {
 	return (
 		<div className="flex h-full items-center justify-center pt-8">
-			<Loader2 className="animate-spin" />
+			{/* <Loader2 className="animate-spin" /> */}
+			<div className="h-10 w-10 animate-spin rounded-full border-4 border-blue-500 border-t-transparent" />
 		</div>
 	);
 }
